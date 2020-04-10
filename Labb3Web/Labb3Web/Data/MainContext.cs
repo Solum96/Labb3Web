1a900726b8f7924db0f2c660b396884c12526c30
Nils Lundell <nils.solumfaeste@gmail.com>
2020-04-10 14:17:58 +0200

    Added project, models, context and controller

---
+++ Labb3Web/Labb3Web/Data/MainContext.cs
@@ -0,0 +1,30 @@
+﻿using Labb3Web.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Labb3Web.Data
+{
+    public class MainContext : DbContext
+    {
+        public MainContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Salon> Salons { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Viewing> Viewings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().ToTable("Movies");
+            modelBuilder.Entity<Salon>().ToTable("Salons");
+            modelBuilder.Entity<Ticket>().ToTable("Tickets");
+            modelBuilder.Entity<Viewing>().ToTable("Viewings");
+        }
+    }
+}
